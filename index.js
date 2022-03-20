e5c867f9eda0ff53af8aa39f33b0761881ed187b
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2022-03-20 11:26:26 +0530

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let ulll = document.getElementById('ull')
+let ham = document.getElementById('ham')
+console.log(ulll)
+ham.addEventListener('click',()=>{
+    ulll.classList.toggle('hidden')
+    ulll.classList.toggle('flex')
+})
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      width: {
+        '90%': '90%',
+      }
+    },
+    // screens: {
+
+    //   'md': '882px',
+    //   'lg':'1024px'
+    // },
+  },
+  plugins: [],
+}
